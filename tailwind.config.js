11fc457df4759f9fa73453d1b896d4fc91d68844
unknown <opusberedugo@gmail.com>
2024-06-11 13:58:47 +0100

    Menu Responsiveness, Contact page etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '150px',  // Extra small devices (very small phones, starting from 150px)
+        'sm': '640px',  // Small devices (landscape phones, starting from 640px)
+        'md': '768px',  // Medium devices (tablets, starting from 768px)
+        'lg': '1024px', // Large devices (desktops, starting from 1024px)
+        'xl': '1280px', // Extra large devices (large desktops, starting from 1280px)
+        '2xl': '1536px' // 2XL large devices (larger desktops, starting from 1536px)
+      }
+    },
     colors: {
       transparent: 'transparent',
       white: '#ffffff',
